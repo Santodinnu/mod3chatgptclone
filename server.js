193026b1b6daf4c6a61e1650f74fc587373e9941
santodinnu <shantoshi@gmail.com>
2024-01-28 23:46:09 -0500

    imported error handler in server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const colors = require("colors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const errorHandler = require("./middelwares/errorMiddleware");
+
 
 //routes path
 const authRoutes = require("./routes/authRoutes");
@@ -22,7 +24,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-// app.use(errorHandler);
+app.use(errorHandler);
 
 
 const PORT = process.env.PORT || 8080
