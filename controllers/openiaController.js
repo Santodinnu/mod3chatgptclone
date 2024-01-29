2777ff6df1d3b6ffdbd72bb4750eb12260cdc606
santodinnu <shantoshi@gmail.com>
2024-01-29 11:29:29 -0500

    create call back function for summary

--- controllers/openiaController.js
+++ controllers/openiaController.js
@@ -4,9 +4,33 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+
+exports.summaryController = async (req, res) => {
+    try {
+      const { text } = req.body;
+      const { data } = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `Summarize this \n${text}`,
+        max_tokens: 500,
+        temperature: 0.5,
+      });
+      if (data) {
+        if (data.choices[0].text) {
+          return res.status(200).json(data.choices[0].text);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+      return res.status(404).json({
+        message: err.message,
+      });
+    }
+  };
+
 const response = await openai.createCompletion({
 model: "text-davinci-003",
 prompt: "Say this is a test",
-max_token: 7,
+max_tokens: 7,
 temperature: 0,
 });
