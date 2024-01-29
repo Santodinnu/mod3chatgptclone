784cfd2209134e631812d11d9fbdf4a68c35b481
santodinnu <shantoshi@gmail.com>
2024-01-28 22:23:30 -0500

    config dotenv

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const bodyParser = require("body-parser");
 const colors = require("colors");
 const dotenv = require("dotenv");
 
+//dotenv
+dotenv.config();
+
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(errorHandler);
 
 app.listen(8080, () => {
     console.log("Server running on port 8080");
