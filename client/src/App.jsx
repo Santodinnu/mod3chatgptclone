5f5d1e94f7d0a6aab2f834709b4ddedaf21ed613
santodinnu <shantoshi@gmail.com>
2024-01-29 10:57:09 -0500

    got api key from open ai

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,29 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider} from "@mui/material";
+import { createTheme } from "@mui/material/styles"
 
 
+import { themeSettings } from "./theme.js";
 
 import Navbar from "./components/Navbar.jsx";
 import Homepage from "./pages/Homepage.jsx";
 
-
 function App() {
-  // const theme = useMemo(() => createTheme(themeSettings()), []);
+  const theme = useMemo(() => createTheme(themeSettings(), []));
 
   return (
     <>
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Navbar />
-        
+
         <Routes>
           <Route path="/" element={<Homepage />} />
-          
         </Routes>
-      </>
+      </ThemeProvider>
+    </>
   );
 }
 export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
-import { Box, Typography, Link } from "@mui/material";
-import { purple } from '@mui/material/colors';
-import { NavLink } from 'react-router-dom';
+import { Box, Typography, Link, useTheme} from "@mui/material";
+
 
 const Navbar = () => {
+   const theme = useTheme()
   return (
 
 
     <Box
     width={"100%"}
+    backgroundColor={theme.palette.background.alt}
     p="1rem 6%"
     textAlign={"center"}
-    sx={{ boxShadow: 3, mb: 2, bgcolor: 'purple', color:'white' }}
+    sx={{ boxShadow: 3, mb: 2}}
   >
-    <Typography variant='h1' color={'white'} fontWeight={'bold'}>
+    <Typography variant='h1' color={'primary'} fontWeight={'bold'}>
       AI GPT Clone
     </Typography>
-    <NavLink to ='/register' p={1}>Sign Up</NavLink>
-    <NavLink to ='/login' p={1}>Sign In</NavLink>
+    <Link href ='/register' p={1}>Sign Up</Link>
+    <Link href ='/login' p={1}>Sign In</Link>
       
     </Box>
       
